04071276abefe8c3a14fbaae98229f7fa5314456
jhall1113 <jhall1113@gmail.com>
2025-08-11 15:52:48 -0400

    Initial commit

---
+++ regular test.py
@@ -0,0 +1,10 @@
+import numpy as np
+print('hello')
+# Creating a 1D array
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+# Creating a 2D array
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr_2d)
+
