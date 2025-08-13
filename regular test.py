6e7585a96a950ce3632cb4ad5da722dac54108c4
jhall1113 <jhall1113@gmail.com>
2025-08-13 11:46:31 -0400

    just greating some code similar to what is in main_ml, to try and better understand its process

--- regular test.py
+++ regular test.py
@@ -1,11 +1,22 @@
 import numpy as np
-print('hello')
-# Creating a 1D array
-arr = np.array([1, 2, 3, 4, 5])
-print(arr)
+class FullProcess:
+    def __init__(self):
+        self.attribute1 = '1'
+        self.attribute2 = '2'
 
-# Creating a 2D array
-arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
-print(arr_2d)
+    def method1(self):
+        meth1attribute = 'M1A'
+        meth2attribute = self.attribute2
+        print("1st attribute in method 1 is unique to method 1",meth1attribute)
+        print("2nd attribute in method 1 is the class attribute #2",meth2attribute)
 
-print('testing new branch change')
+def download_method(parameter_download):
+    class_call = FullProcess()
+    print("main class attribute called from download method",class_call.attribute1)
+    print("location of this method call was in ",parameter_download)
+    # print("now the method 1 is called, with this as the result ==> ",class_call.method1())
+if __name__ == '__main__':
+    print('in main, creating class object')
+    in_main_class_call = FullProcess()
+    print("class attribute called from main==>", in_main_class_call.attribute1)
+    download_method('from main')
